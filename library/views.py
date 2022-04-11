67889e3aa486dafbda26a63a09ec58cd1a543b98
CleRIeQ <aleksandrsd006@gmail.com>
2022-04-11 06:56:21 +0600

    study-test project

---
+++ library/views.py
@@ -0,0 +1,7 @@
+from typing import List
+from django.views.generic import ListView
+from .models import Book
+
+class BookListView(ListView):
+    model = Book
+    template_name = 'books/book_list.html'
